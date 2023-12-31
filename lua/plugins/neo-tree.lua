85e20d0e92920a438365a7264ea759f8ad680ce6
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2023-12-30 21:59:42 -0800

    fix: update neo-tree to show dotfiles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,11 +2,22 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
     config = function()
         vim.keymap.set("n", "<Leader>nn", ":Neotree<CR>", {})
+
+        local nt = require("neo-tree")
+
+        nt.setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                }
+            }
+        })
     end
 }
