fb248145ad63f3ca47d635e7154b6c706721023c
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-01-29 14:18:49 -0800

    feat: add binding for git checkout

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,5 +5,6 @@ return {
         vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>", { desc = "Pick gitdiff on left" })
         vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>", { desc = "Pick gitdiff on right" })
         vim.keymap.set("n", "<Leader>gp", ":Git push<CR>", { desc = "Run `git push`" })
+        vim.keymap.set("n", "<Leader>gc", ":Git checkout ", { desc = "Git checkout" })
     end
 }
