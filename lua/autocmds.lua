ddcd313eef7aaea8561c6d249df5532100219ea7
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-02-08 19:05:55 -0800

    fix: remove autocmd for quit on q
    This was causing issues with fugitive and committing changes via git.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,21 +1,21 @@
 local function augroup(name)
-    return vim.api.nvim_create_augroup("wf_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("wf_" .. name, { clear = true })
 end
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    group = augroup("spellcheck"),
-    pattern = { "gitcommit", "markdown", "md", "mdx" },
-    callback = function()
-        vim.opt_local.spell = true
-        vim.opt_local.wrap = true
-    end
+	group = augroup("spellcheck"),
+	pattern = { "gitcommit", "markdown", "md", "mdx" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.wrap = true
+	end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    group = augroup("close_with_q"),
-    pattern = { "fugitive", "git", "gitcommit" },
-    callback = function(event)
-        vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    end
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--     group = augroup("close_with_q"),
+--     pattern = { "fugitive", "git", "gitcommit" },
+--     callback = function(event)
+--         vim.bo[event.buf].buflisted = false
+--         vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+--     end
+-- })
