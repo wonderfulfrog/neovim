30c47293b0132ec28fdc433e8a0f7929f39052e4
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-04-01 11:04:10 -0700

    fix: disable q to quit certain filetypes

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -11,11 +11,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = augroup("close_with_q"),
-	pattern = { "fugitive", "git", "gitcommit" },
-	callback = function(event)
-		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+-- 	group = augroup("close_with_q"),
+-- 	pattern = { "fugitive", "git", "gitcommit" },
+-- 	callback = function(event)
+-- 		vim.bo[event.buf].buflisted = false
+-- 		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+-- 	end,
+-- })
