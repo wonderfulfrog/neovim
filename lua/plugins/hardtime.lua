f7100e78719d45658850bc99b094033ca47db13b
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-04-01 10:53:29 -0700

    feat: ignore hardtime inside markdown

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,26 +1,27 @@
 return {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {
-        disable_mouse = true,
-        disabled_keys = {
-            -- Drill Sergeant Demchuk!
-            ["<Up>"] = { "", "i" },
-            ["<Down>"] = { "", "i" },
-            ["<Left>"] = { "", "i" },
-            ["<Right>"] = { "", "i" }
-        },
-        disabled_filetypes = {
-            "checkhealth",
-            "fugitive",
-            "help",
-            "lazy",
-            "mason",
-            "neo-tree",
-            "neo-tree-popup",
-            "netrw",
-            "noice",
-            "notify",
-        }
-    }
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disable_mouse = true,
+		disabled_keys = {
+			-- Drill Sergeant Demchuk!
+			["<Up>"] = { "", "i" },
+			["<Down>"] = { "", "i" },
+			["<Left>"] = { "", "i" },
+			["<Right>"] = { "", "i" },
+		},
+		disabled_filetypes = {
+			"checkhealth",
+			"fugitive",
+			"help",
+			"lazy",
+			"mason",
+			"markdown",
+			"neo-tree",
+			"neo-tree-popup",
+			"netrw",
+			"noice",
+			"notify",
+		},
+	},
 }
