6829dbbb06dfcba2c2e5c365dcd51b5a117ae7cd
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-04-26 17:00:15 -0600

    feat: small tweak to colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-        transparent = true,
-        dim_inactive = true,
-    }
+	"folke/tokyonight.nvim",
+	lazy = true,
+	opts = {
+		style = "storm",
+		transparent = true,
+		dim_inactive = true,
+	},
 }
