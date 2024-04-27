5e93f497de72808557fd2f686063f295e47314c7
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-04-26 23:55:34 -0600

    feat: update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,41 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"lua",
-					"javascript",
-					"typescript",
-					"tsx",
-					"htmldjango",
-					"html",
-					"jsonc",
-					"json",
-					"jsdoc",
-					"markdown",
-					"markdown_inline",
-					"vim",
-					"vimdoc",
-				},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		opts = {
-			max_lines = 2,
-		},
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {},
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false,
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = {
+                "lua",
+                "javascript",
+                "typescript",
+                "tsx",
+                "htmldjango",
+                "html",
+                "jsonc",
+                "json",
+                "jsdoc",
+                "markdown",
+                "markdown_inline",
+                "vim",
+                "vimdoc",
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {
+            max_lines = 2,
+        },
+    },
+    {
+        -- TODO: Figure out why this plugin is not working.
+        "windwp/nvim-autopairs",
+        event = "VeryLazy",
+        opts = {},
+    },
 }
