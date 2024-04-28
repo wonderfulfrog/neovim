8a81569687fc5716883053cda3d06b32ca110e18
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-04-27 18:48:05 -0700

    feat: move all UI plugins to `ui.lua`

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,55 +1,156 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		opts = {
-			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			scope = { enabled = false },
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-			},
-		},
-		main = "ibl",
-	},
-	{
-		"echasnovski/mini.indentscope",
-		opts = {
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = "tokyonight",
+                disabled_filetypes = {
+                    statusline = {
+                        "alpha",
+                        "neo-tree",
+                    }
+                }
+            }
+        },
+    },
+    {
+        "akinsho/bufferline.nvim",
+        event = "VeryLazy",
+        keys = {
+            { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+            { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+        },
+        opts = {
+            options = {
+                diagnostics = "nvim_lsp",
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "Neo-tree",
+                        highlight = "Directory",
+                        text_align = "left",
+                    },
+                },
+            }
+        }
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { enabled = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+            },
+        },
+        main = "ibl",
+    },
+    {
+        "echasnovski/mini.indentscope",
+        opts = {
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
+    {
+        "stevearc/dressing.nvim",
+        event = "VeryLazy",
+        opts = {}
+    },
+    {
+        "rcarriga/nvim-notify",
+        opts = {}
+    },
+    {
+        "MunifTanjim/nui.nvim"
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {},
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        }
+    },
+    {
+        "goolord/alpha-nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+
+            -- https://github.com/MaximilianLloyd/ascii.nvim
+            dashboard.section.header.val = {
+                "                                                                          ",
+                "                   ░░░  ▄▄▄▄▄▄███▄▄▄▄▄ ",
+                "                    ▄███████▀▀▀▀▀▀█▓▓▓▓▓▓██▄▄▄▄ ",
+                "                   ▐████▄▄    ■▓▓▓▓▀▀▀▀▀▀▀▀▀█████■                ██▄ ",
+                "                    ▀█████████▄▄▄▄▄███████████▀▀             ▄▄  ████░▄▄▄ ",
+                "    ▐██▄            ▄▄▄▄▄▄▀▀▀▀▀▀▀▀▓▓▓▀░░░▄▄▄   ▄▄          ▐███▏▐██▓▄████ ",
+                " ▄█▓░███▓ ▄▓▓▓▄▄████████████▄█████▄ ▄█████████▐███         ████▏██▀████▀ ",
+                " ▐███▄███░▀████████▀▀   ▀▓█████▓▀██▓████▀▀  ███████      ▄█████ ■ ██▀ ",
+                "  ▀████▓██ ▐██████       ▐████   ▐████▀    ▒████████▄  ▄███████ ",
+                "    ▀███    ▀▀█████▄    ▄████   ▄████▏    ▄█████▀█████████▀ ███ ",
+                "               ▀████▄ ▄████▐██▄███████▄▄▄███▓██▏ ▀██████▀   ▐██ ",
+                "                ▀████████▀  ▀████▀▀████████▀▐██▏  ▀██▀▀     ▐██▏ ",
+                "          ▓▓█    ▀████ ▀            ■███▄▄  ███              ██▏ ",
+                "          ▀▀▀      ▀▀                ▀█████▄▓▀               ██▏ ",
+                "            ░░     ▄████████████▄▄▄▄▄▄▄ ▀▀▀████▄▄   ▒▒▒      ██▏  ▄▄ ",
+                "                   ▀▀████▀▀▀▀▀▀████████████▄▄▓▓▓███▄    ░░░░ ▐█▏ ■▀▀ ",
+                "                                      ▀▀▀▀▀▀█████▓▓▓▓█▄▄     ▐█ ",
+                "                                                ▄█████████▄▄ ",
+                "                                       ▄▄▄▄████████▀▀▀▀▀▀▀███▏ ",
+                "                                   ▄▄█████▀▀▀▀              ▀■ ",
+                "                                ▄███▀▀▀▀ ",
+                "                              ■▀▀ ",
+                "                                                                          ",
+            }
+
+            dashboard.section.buttons.val = {
+                dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+                dashboard.button("SPC ee", "  > File explorer", "<cmd>Neotree<CR>"),
+                dashboard.button("q", "  > Quit", "<cmd>qa<CR>"),
+            }
+
+            alpha.setup(dashboard.opts)
+        end,
+    }
 }
