3f76f98d4799b3bddeda8f23505e99ae15d2d4ff
Devin Haska <wonderfulfrog@users.noreply.github.com>
2024-05-02 11:27:13 -0700

    feat: update js formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,6 +18,7 @@ return {
         opts = {
             ensure_installed = {
                 "djlint",
+                "eslint",
                 "eslint_d",
                 "prettierd",
                 "stylua",
