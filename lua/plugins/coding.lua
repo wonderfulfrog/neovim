eb0b2e9bccadf05c63980b204396928fa6135027
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-06-06 20:48:25 -0700

    feat: small tweak to nvim-cmp menu

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -14,6 +14,12 @@ return {
 				completion = {
 					completeopt = "menu,menuone,noinsert",
 				},
+				window = {
+					completion = cmp.config.window.bordered({
+						side_padding = 1,
+						scrollbar = false,
+					}),
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
