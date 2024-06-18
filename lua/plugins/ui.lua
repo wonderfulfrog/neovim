1494a321f6ab614af9d0003b4a21ac594092616e
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-06-17 17:58:17 -0700

    feat: update lualine config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,6 +8,21 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "tokyonight",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true,
+						path = 1,
+						symbols = {
+							modified = "●",
+						},
+					},
+				},
+				lualine_x = { "filetype" },
 			},
 		},
 	},
