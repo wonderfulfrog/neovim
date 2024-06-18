36de7c527d9a0dd6b0e0f6f88296fa9e12180873
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-06-17 21:21:13 -0700

    feat: add toml formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -11,11 +11,26 @@ return {
 			javascriptreact = { "prettierd" },
 			json = { "prettierd" },
 			jsonc = { "prettierd" },
+			toml = { "taplo" },
 			typescript = { "prettierd" },
 			typescriptreact = { "prettierd" },
 		},
 		format_on_save = {
 			lsp_fallback = true,
+		},
+		formatters = {
+			taplo = {
+				args = function()
+					-- conform.nvim does not parse bash envvars, so we do it ourselves
+					local CONFIG_PATH = os.getenv("HOME") .. "/.config/taplo/taplo.toml"
+					return {
+						"format",
+						"--config",
+						CONFIG_PATH,
+						"-",
+					}
+				end,
+			},
 		},
 	},
 }
