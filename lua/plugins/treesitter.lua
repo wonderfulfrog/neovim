ad4dfe73ea558d3ae428949dd5a9a564621bd967
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-06-22 21:14:52 -0700

    feat: disable treesitter-context
    This plugin is flooding my editor with errors when editing Markdown
    files. Disabling for now until I can look into it more.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -26,12 +26,12 @@ return {
 			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		opts = {
-			max_lines = 2,
-		},
-	},
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter-context",
+	-- 	opts = {
+	-- 		max_lines = 2,
+	-- 	},
+	-- },
 	{
 		"windwp/nvim-ts-autotag",
 		event = "VeryLazy",
