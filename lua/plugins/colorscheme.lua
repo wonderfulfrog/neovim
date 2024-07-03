e40388a48ffb21f14a61b787c18971c38d1d5073
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-07-02 17:53:54 -0700

    chore: remove catppuccin colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,40 +8,4 @@ return {
 			terminal_colors = true,
 		},
 	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			default_integrations = true,
-			dim_inactive = {
-				enabled = true,
-			},
-			flavour = "macchiato",
-			integrations = {
-				alpha = true,
-				cmp = true,
-				flash = true,
-				gitsigns = true,
-				lsp_trouble = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				neotree = true,
-				noice = true,
-				telescope = {
-					enabled = true,
-				},
-				treesitter = true,
-				treesitter_context = true,
-				which_key = true,
-			},
-		},
-	},
 }
