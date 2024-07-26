b63351398d00d87aaa61f7bab1d693fc8398a88f
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-07-25 22:41:09 -0700

    feat: add keybinds for staging/resetting hunks

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -172,6 +172,18 @@ return {
 				changedelete = { text = "▎" },
 				untracked = { text = "▎" },
 			},
+			signs_staged = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+			},
+		},
+		keys = {
+			{ "<leader>ghb", ":Gitsigns blame_line<CR>", desc = "Blame line" },
+			{ "<leader>ghs", ":Gitsigns stage_hunk<CR>", desc = "Stage hunk", mode = { "n", "v" } },
+			{ "<leader>ghr", ":Gitsigns reset_hunk<CR>", desc = "Reset hunk", mode = { "n", "v" } },
 		},
 	},
 	{
