e5136f66df21386771fef299fd7cc768e454b00a
Devin Haska <2636402+wonderfulfrog@users.noreply.github.com>
2024-09-15 20:16:43 -0700

    fix: clean up keybinds
    Previous keybinds for flash.nvim were conflicting with
    tpope/vim-surround.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,11 +14,11 @@ return {
 		opts = {},
         -- stylua: ignore
         keys = {
-            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+            { "<Leader>s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "<Leader>S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "<Leader>r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "<Leader>R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<Leader><c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
 	},
 	{
